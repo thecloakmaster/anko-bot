22c3e506ad5ee896e23e70555059af94b7ed65e1
Nisarg Desai <51017498+thecloakmaster@users.noreply.github.com>
2022-02-01 10:33:17 +0400

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,23 @@ for (const file of commandFiles) {
 	client.commands.set(command.name, command);
 }
 
+client.on('guildMemberAdd', member => {
+	const welcomeEmbed = new Discord.MessageEmbed()
+		.setColor("#00ffff")
+		.setTitle(`Welcome to Yofukashi no Uta!`)
+		.setDescription(`Be sure to check out <#908021113504825358> and <#908021113504825361> before you start chatting. Enjoy your stay here!`)
+		.setImage(`https://images-ext-1.discordapp.net/external/c5ZtuDzOH1WQlK-1YTGBEvxffw2PhCuWKb77dYjIcYY/https/media.discordapp.net/attachments/912098426924183602/920996161878392882/N47QgJE.gif`)
+		.setFooter({
+			text: `There are currently ${member.guild.memberCount} vampires here.`
+		})
+
+	const channel = member.guild.channels.cache.get('908021113886482434')
+	channel.send({
+		content: `Welcome to the server <@${member.user.id}>!`,
+		embeds: [welcomeEmbed]
+	})
+})
+
 client.on('message', message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
