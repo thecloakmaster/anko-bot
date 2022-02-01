ff774172b00e8d9bc297233cdcc8eee10322ed89
Nisarg Desai <51017498+thecloakmaster@users.noreply.github.com>
2022-02-01 10:55:45 +0400

    Create archivepins.js

---
+++ commands/archivepins.js
@@ -0,0 +1,74 @@
+const {
+    MessageEmbed,
+    Guild,
+    Message
+} = require('discord.js');
+module.exports = {
+    name: 'archivepins',
+    description: "Takes pins from a channel and archives them to elsewhere",
+    async execute(message) {
+
+        if (!message.member.permissions.has("MANAGE_MESSAGES")) {
+            const permerror = new MessageEmbed()
+                .setColor("RED")
+                .setTitle(`Error executing that command`)
+                .setDescription(`You do not have the necessary permissions to execute this command`)
+                .setTimestamp();
+            return message.reply({
+                embeds: [permerror]
+            });
+        };
+
+        message.channel.send("Archiving pins....");
+        
+        const pinFetch = await message.channel.messages.fetchPinned();
+
+        const pinnedMessages = Array.from(pinFetch.values());
+        try {
+            for (var i = 0; i < pinnedMessages.length; i++) {
+                let pin = pinnedMessages[i];
+                var color = "#FFFFFF"
+                if (!message.guild.members.cache.get(pin.author.id)) {
+                    var color = "#FFFFFF"
+                } else {
+                    var color = pin.member.displayHexColor;
+                };
+
+                if (pin.attachments.size > 0) {
+                    pin.attachments.forEach(attachment => {
+                        let imageURL = attachment.proxyURL;
+                        let embed = new MessageEmbed()
+                            .setColor(color)
+                            .setDescription(`${pin.content} \n\n[Jump to message](${pin.url})`)
+                            .setImage(imageURL)
+                            .setTimestamp(pin.createdTimestamp)
+                            .setAuthor({name:pin.author.username, iconURL: pin.author.displayAvatarURL()});
+                        message.channel.send({
+                            embeds: [embed]
+                        })
+                        pin.unpin()
+                    })
+                } else {
+                    let embed = new MessageEmbed()
+                        .setColor(color)
+                        .setDescription(`${pin.content} \n\n[Jump to message](${pin.url})`)
+                        .setAuthor({name: pin.author.username, iconURL: pin.author.displayAvatarURL()})
+                        .setTimestamp(pin.createdTimestamp);
+                    message.channel.send({
+                        embeds: [embed]
+                    })
+                    pin.unpin()   
+                }
+            }
+            const afterArchive = new MessageEmbed()
+            .setColor("#97e0ff")
+            .setDescription(`**The pin archiving has been completed successfully.** \n\n [Go to the first archived message.](${message.url})`)
+            .setTimestamp();
+            message.channel.send({embeds: [afterArchive]});
+
+        } catch(err) {
+            console.log(err)
+        }
+        
+    }
+}
