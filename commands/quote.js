2925fd84e12478622a9811b8553efa726b373143
Nisarg Desai <51017498+thecloakmaster@users.noreply.github.com>
2022-02-02 13:09:34 +0400

    yems

--- commands/quote.js
+++ commands/quote.js
@@ -8,19 +8,60 @@ module.exports = {
     description: 'Embeds the mentioned message.',
     async execute(message, args) {
 
-        const channelID = message.mentions.channels.first();
-
-        if (!channelID) return message.channel.send("Please specify a valid channel.");
-
-        const messageID = args.slice(1).join(" ");
+        let potentialID = args[0]
 
         let messageW = null
 
         try {
-            messageW = await channelID.messages.fetch(`${messageID}`);
-        } catch (err) {
-            return message.channel.send("Please enter a valid message ID")
-        }
+            messageW = await message.channel.messages.fetch(`${potentialID}`)
+            try {
+                const inServer = await message.guild.members.fetch(messageW.author.id).catch(() => {});
+                if (!inServer) {
+                    var color = "#4752c4"
+                } else {
+                    var color = messageW.member.displayHexColor;
+                    console.log(color)
+                };
+
+                if (messageW.attachments.size > 0) {
+                    const embed = new MessageEmbed()
+                        .setColor(color)
+                        .setDescription(`${messageW.content} \n\n[Jump to message](${messageW.url})`)
+                        .setImage(imageURL)
+                        .setTimestamp(messageW.createdTimestamp)
+                        .setAuthor({
+                            name: messageW.author.username,
+                            iconURL: messageW.author.displayAvatarURL()
+                        });
+                    return message.channel.send({
+                        embeds: [embed]
+                    })
+                } else {
+                    const embed = new MessageEmbed()
+                        .setColor(color)
+                        .setDescription(`${messageW.content} \n\n[Jump to message](${messageW.url})`)
+                        .setAuthor({
+                            name: messageW.author.username,
+                            iconURL: messageW.author.displayAvatarURL()
+                        })
+                        .setTimestamp(messageW.createdTimestamp);
+                    return message.channel.send({
+                        embeds: [embed]
+                    })
+                }
+
+            } catch (err) {
+                console.log(err)
+            }
+        } catch (err) {}
+
+        const channelID = message.mentions.channels.first();
+        
+        if (!channelID) return message.channel.send("Please specify a valid channel or ID.");
+
+        const messageID = args.slice(1).join(" ");
+
+        try {messageW = await channelID.messages.fetch(`${messageID}`);} catch(err) {return message.channel.send("Please enter a valid message ID")}
 
         if (!messageW) return message.channel.send("Please specify a valid message ID");
 
@@ -30,9 +71,10 @@ module.exports = {
                 var color = "#4752c4"
             } else {
                 var color = messageW.member.displayHexColor;
+                console.log(color)
             };
-
-            if (messageW.attachments.size > 0) {
+            
+            if(messageW.attachments.size > 0) {
                 const embed = new MessageEmbed()
                     .setColor(color)
                     .setDescription(`${messageW.content} \n\n[Jump to message](${messageW.url})`)
