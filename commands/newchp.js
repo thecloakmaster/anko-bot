0ac30889bd4bcd2389d048f07b6732ed58c6b188
Nisarg Desai <51017498+thecloakmaster@users.noreply.github.com>
2022-02-19 10:46:18 +0400

    Update newchp.js

--- commands/newchp.js
+++ commands/newchp.js
@@ -10,21 +10,23 @@ module.exports = {
         try {
             const redURL = args[1];
             const chpNumber = args[0];
-            const channelID = message.guild.channels.cache.find(chn => chn.id === '908021114138132510')
+            const channelID = message.guild.channels.cache.find(chn => chn.id === '908021114138132510');
             let pingRole = message.guild.roles.cache.find(role => role.name === 'Manga Release');
             const file = new MessageAttachment('./assets/vol.jpg');
             let mangaID = message.guild.channels.cache.find(chn => chn.name === 'ynu-manga')
+
             const chpEmbed = new MessageEmbed()
                 .setThumbnail('attachment://vol.jpg')
                 .setAuthor({
                     name: `Yofukashi no Uta - Chapter ${chpNumber}`,
                     iconURL: client.user.displayAvatarURL()
                 })
-                .setDescription(`A new manga chapter has just been released. The chapter is available on MangaDex and the link can be found on the Reddit post. Use <#${mangaID.id}> to discuss the chapter. 
+                .setDescription(`A new manga chapter has just been released. The chapter is available on MangaDex and the link can be found on the reddit post. Use <#${mangaID.id}> to discuss the chapter. 
         \n*Make sure to use spoilers to talk about the latest chapter to not spoil it for someone else. Enjoy the chapter!*`)
                 .addField('Reddit Link', `[Click here!](${redURL})`)
                 .setColor("#e4a353")
             channelID.setName(`ch-${chpNumber}-info`)
+
             return channelID.send({
                 content: `<@&${pingRole.id}>`,
                 embeds: [chpEmbed],
