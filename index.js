d94dd9b0cea421ec19ab2c3f9c04afd19c92a8bf
Nisarg Desai <51017498+thecloakmaster@users.noreply.github.com>
2022-03-02 10:59:06 +0400

    Update index.js

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ stream.on('tweet', function (tweet) {
 
 client.on('messageCreate', message => {
 	if (message.channel.type === 'DM') {
-		if (message.author === client)
+		if (message.author === client) return;
 		const embed = new Discord.MessageEmbed()
 			.setColor("#e4a353")
 			.setTitle(`ID: [${message.author.id}]`)
