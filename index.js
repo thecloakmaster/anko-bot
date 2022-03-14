9010c8c6d0a3399bf1079672b66fb710debd3231
Nisarg Desai <51017498+thecloakmaster@users.noreply.github.com>
2022-03-14 10:58:16 +0400

    unmuteCheck

--- index.js
+++ index.js
@@ -8,70 +8,7 @@ const twitFunction = require(`./functions/twit.js`)
 twitFunction.execute();
 
 const unmuteCheck = require(`./functions/unmuteCheck.js`)
-//setInterval(unmuteCheck.execute(client), 30*60*1000)
-setInterval(async function () {
-	try {
-		let muteRole = await MuteRole.find()
-		for (const muteRoleLoop of muteRole) {
-			let guildID = muteRoleLoop.GuildID;
-			let guild = await client.guilds.fetch(`${guildID}`).catch(() => {});
-			let muteDeletes = await MutedMember.find({
-				GuildID: guildID
-			})
-			if (!guild) {
-				await MuteRole.findOneAndRemove({
-					GuildID: guildID
-				})
-				for (memberMuteDelete in muteDeletes) {
-					await MutedMember.findOneAndRemove({
-						GuildID: guildID
-					})
-				}
-			}
-			let mutedRole = muteRoleLoop.MuteRoleID;
-			let guildMuteRole = await guild.roles.fetch(`${mutedRole}`).catch(() => {});
-			if (!guildMuteRole) {
-				await MuteRole.findOneAndRemove({
-					GuildID: guildID,
-					MuteRoleID: mutedRole
-				})
-				for (memberMuteDelete in muteDeletes) {
-					await MutedMember.findOneAndRemove({
-						UserID: memberMuteDelete.UserID,
-						GuildID: guildID
-					})
-				}
-			}
-			let mutedMember = await MutedMember.find({
-				GuildID: guildID
-			})
-			for (const mutedMemberLoop of mutedMember) {
-				let member = await guild.members.fetch(mutedMemberLoop.UserID).catch(() => {});
-				if (!member) {
-					if (Date.now() >= mutedMemberLoop.LastsTill) {
-						await MutedMember.findOneAndRemove({
-							UserID: mutedMemberLoop.UserID,
-							GuildID: mutedMemberLoop.GuildID,
-							LastsTill: mutedMemberLoop.LastsTill
-						})
-					}
-					continue;
-				} else if (member) {
-					if (Date.now() >= mutedMemberLoop.LastsTill) {
-						await MutedMember.findOneAndRemove({
-							UserID: mutedMemberLoop.UserID,
-							GuildID: mutedMemberLoop.GuildID,
-							LastsTill: mutedMemberLoop.LastsTill
-						})
-					}
-					await member.roles.remove(mutedRole).catch((err) => console.log(err))
-				}
-			}
-		}
-	} catch (err) {
-		console.log(err)
-	}
-}, 30 * 60 * 1000)
+unmuteCheck.execute(client)
 
 client.commands = new Discord.Collection();
 client.handlers = new Discord.Collection();
