7919e805cdaafa8874259185936b272b384007f1
Nisarg Desai <51017498+thecloakmaster@users.noreply.github.com>
2022-03-15 08:55:05 +0400

    Update boostRoleIcon.js

--- functions/boostRoleIcon.js
+++ functions/boostRoleIcon.js
@@ -50,7 +50,14 @@ module.exports = {
                 })
                 return console.log(err)
             });
-            await message.member.roles.add(roleID).catch(() => {
+            await message.member.roles.add(roleID).then(() => {
+                let roleEmbed2 = new MessageEmbed()
+                    .setColor(`${roleColour}`)
+                    .setDescription(`The role <@&${roleID}> has been created and applied to <@${message.author.id}>`)
+                return roleEmbedMessage.edit({
+                    embeds: [roleEmbed2]
+                })
+            }).catch(() => {
                 let roleEmbed2 = new MessageEmbed()
                     .setColor(`#e4a353`)
                     .setDescription(`An error occured while creating this role. Please provide a valid image. (The image should be a .png or a .jpg file and should be smaller than 256 KB.)`)
@@ -58,12 +65,6 @@ module.exports = {
                     embeds: [roleEmbed2]
                 })
             });
-            let roleEmbed2 = new MessageEmbed()
-                .setColor(`${roleColour}`)
-                .setDescription(`The role <@&${roleID}> has been created and applied to <@${message.author.id}>`)
-            return roleEmbedMessage.edit({
-                embeds: [roleEmbed2]
-            })
         } catch (err) {
 
         }
