86378c7ca171f7134648bab8712d13330841ac3a
Nisarg Desai <51017498+thecloakmaster@users.noreply.github.com>
2022-03-18 13:09:42 +0400

    emojis

--- commands/mod utilities/addsticker.js
+++ commands/mod utilities/addsticker.js
@@ -7,9 +7,9 @@ module.exports = {
     aliases: ['addst', 'stickeradd', 'addsticker'],
     async execute(message, args, client) {
         const bot = await message.guild.members.fetch(`${client.user.id}`)
-        if (!message.member.permissions.has("MANAGE_EMOTES_AND_STICKERS")) {
+        if (!message.member.permissions.has("MANAGE_EMOJIS_AND_STICKERS")) {
             return message.channel.send(`You do not have the necessary permissions to execute this command.\nPermissions required: \`MANAGE_EMOTES_AND_STICKERS\``)
-        } else if (!bot.permissions.has("MANAGE_EMOTES_AND_STICKERS")) {
+        } else if (!bot.permissions.has("MANAGE_EMOJIS_AND_STICKERS")) {
             return message.channel.send(`I do not have the necessary permissions to execute this command.\nPermissions required: \`MANAGE_EMOTES_AND_STICKERS\``)
         }
         if (message.guild.premiumTier === `NONE`) {return message.channel.send(`This server has no boosts and hence no stickers can be added.`)}
