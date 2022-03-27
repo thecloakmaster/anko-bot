65f2ac33eb31cc4666788abdd99f22a5b3a85dfa
Nisarg Desai <51017498+thecloakmaster@users.noreply.github.com>
2022-03-27 21:56:05 +0400

    Update spank.js

--- commands/utilities/spank.js
+++ commands/utilities/spank.js
@@ -1,65 +1,53 @@
-const {
-    MessageEmbed
-} = require("discord.js");
-
-const ms = require("ms");
+const {MessageEmbed} = require("discord.js");
 
 module.exports = {
     name: 'spank',
     description: 'Spanks the specified member and mutes them for 1 minute.',
     usage: ";spank @mention",
     aliases: ["shut"],
-    async execute(message, args) {
-        let spankRole = message.guild.roles.cache.find(role => role.name === 'Spanked');
-        if (!message.member.permissions.has("MANAGE_ROLES")) {
+    async execute(message, args, client) {
+        const bot = await message.guild.members.fetch(`${client.user.id}`)
+        if (!message.member.permissions.has("MODERATE_MEMBERS")) {
             try {
-                message.member.roles.add(spankRole.id);
-                setTimeout(function () {
-                    message.member.roles.remove(spankRole.id);
-                }, ms('1m'));
+                message.member.timeout(60000, "No perms :)")
             } catch (err) {}
             const permerror = new MessageEmbed()
-                .setColor("#e4a353")
+                .setColor("#33FFBD")
                 .setTitle(`You got spanked instead.`)
                 .setTimestamp();
             return message.reply({
                 embeds: [permerror]
             });
+        } else if (!bot.permissions.has(`MODERATE_MEMBERS`)) {
+            return message.channel.send(`No perms :(`)
         };
-
         let memberSpank = message.mentions.members.first() || await message.guild.members.fetch(args[0]).catch(() => {});
 
         if (!memberSpank) {
             return message.channel.send("Specify a valid user to be spanked.");
         }
-
-        
-
-        if (memberSpank.roles.cache.some(role => role.name === 'Spanked')) {
-            const alrSpanked = new MessageEmbed()
-                .setColor("#e4a353")
-                .setTitle(`Error executing command.`)
-                .setDescription(`This member has already been spanked.`)
+        let reason = args.slice(1).join(" ")
+        let spankEmbed = null
+        if (reason) {
+            spankEmbed = new MessageEmbed()
+                .setColor("#33FFBD")
+                .setDescription(`<@!${memberSpank.user.id}> has been spanked for ${reason}.`)
                 .setTimestamp();
-
-            return message.reply({
-                embeds: [alrSpanked]
-            });
+        } else {
+            spankEmbed = new MessageEmbed()
+                .setColor("#33FFBD")
+                .setDescription(`<@!${memberSpank.user.id}> has been spanked.`)
+                .setTimestamp();
         }
-
+             
+        if (memberSpank.roles.highest.position > bot.roles.highest.position) {
+            return message.channel.send({embeds: [spankEmbed]})
+        } else if (memberSpank.roles.highest.position > message.member.roles.highest.position) {
+            return message.channel.send({embeds: [spankEmbed]})
+        }
         try {
-            memberSpank.roles.add(spankRole.id);
-            const spankEmbed = new MessageEmbed()
-                .setColor("#e4a353")
-                .setTitle(`${memberSpank.user.tag} has been spanked.`)
-                .setTimestamp();
-            message.channel.send({
-                embeds: [spankEmbed]
-            })
-
-            setTimeout(function () {
-                memberSpank.roles.remove(spankRole.id);
-            }, ms('1m'));
+            memberSpank.timeout(60000, `${reason}`)
+            return message.channel.send({embeds: [spankEmbed]})
         } catch (err) {
             console.log(err);
         }
