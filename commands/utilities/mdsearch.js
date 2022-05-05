60069bd8490bdb809d73a137ffb7a3449f91cf76
Nisarg Desai <51017498+thecloakmaster@users.noreply.github.com>
2022-05-05 15:31:32 +0400

    Update mdsearch.js

--- commands/utilities/mdsearch.js
+++ commands/utilities/mdsearch.js
@@ -17,7 +17,7 @@ module.exports = {
             try {
                 let mangas = await MFA.Manga.search(`${args.slice(0).join(" ")}`)
                 for (let i of mangas) {
-                    options.push({label: `${i.localizedTitle[i.localizedTitle.availableLocales[0]]}`, value: `${i.id}`})
+                    options.push({label: `${i.localizedTitle[i.localizedTitle.availableLocales[0]].substring(0,100)}`, value: `${i.id}`})
                 }
             } catch (err) {
                 console.log(err)
