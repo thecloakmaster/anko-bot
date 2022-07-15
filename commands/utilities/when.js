29f98a810f1bae94231b8c0f02de2ad18602d7e9
cloak <harrypotter200520@gmail.com>
2022-07-15 17:00:47 +0400

    Update when.js

--- commands/utilities/when.js
+++ commands/utilities/when.js
@@ -9,7 +9,7 @@ module.exports = {
             .setAuthor({name: `${client.user.username}`, iconURL: `${client.user.displayAvatarURL({dynamic: true, size: 1024})}`})
             .setColor(`${process.env.colour}`)            
 
-        if (!args || args[0] === 'en' || args[0] === 'en') {
+        if (!args[0] || args[0] === 'eng' || args[0] === 'en') {
             embed.setDescription('The next chapter will be released when the TL team is done with it. There is no specific schedule which is followed for the release of translated English chapters. The TL team appreciates your patience.')            
         } else if (args[0] === 'jp') {
             embed.setDescription('The Japanese raws release every Wednesday at 00:00 JST.')
