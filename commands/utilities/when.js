5e8cc2c1d8594aad0952373f048c9a543118d052
cloak <harrypotter200520@gmail.com>
2022-07-15 16:29:29 +0400

    Update when.js

--- commands/utilities/when.js
+++ commands/utilities/when.js
@@ -2,18 +2,18 @@ const { MessageEmbed } = require("discord.js")
 
 module.exports = {
     name: 'when',
-    description: 'The answer to next chapter when?',
+    description: 'The answer to next chapter/episode when?',
     usage: ';when',
     async execute(message, args, client) {
         let embed = new MessageEmbed()
             .setAuthor({name: `${client.user.username}`, iconURL: `${client.user.displayAvatarURL({dynamic: true, size: 1024})}`})
             .setColor(`${process.env.colour}`)            
 
-        if (!args || args[0] === 'eng') {
+        if (!args || args[0] === 'en' || args[0] === 'en') {
             embed.setDescription('The next chapter will be released when the TL team is done with it. There is no specific schedule which is followed for the release of translated English chapters. The TL team appreciates your patience.')            
         } else if (args[0] === 'jp') {
             embed.setDescription('The Japanese raws release every Wednesday at 00:00 JST.')
-        } else if (args[0] === 'a' || args[0] === 'anime') {
+        } else if (args[0] === 'a' || args[0] === 'anime' || args[0] === 'episode' || args[0] === 'ep') {
             embed.setDescription('The Japanese raws (episode without subtitles) release every Friday at 01:05 JST. The English subtitles usually release an hour later.')
         }
         return message.channel.send({embeds: [embed]})
