e2b659f124c37022753096b04cd544b7fce90b9a
cloak <51017498+thecloakmaster@users.noreply.github.com>
2023-05-22 18:28:18 +0400

    Update when.js

--- commands/utilities/when.js
+++ commands/utilities/when.js
@@ -54,54 +54,53 @@ module.exports = {
     async execute(message, args, client) {
         let mangaTime = await MangaChapter.findOne({GuildID: message.guild.id, ClientID: client.user.id})
         let rawChp = 'The Japanese raws release every Wednesday at 00:00 JST in the Shuukan Shounen Sunday.'
-        let animeTime = 'The Japanese raws (episode without subtitles) release every Friday at 01:05 JST. The English subtitles usually release an hour later.'
-        let animeFetch = await timeTillAiring()
-        if (animeFetch) {
-            if (animeFetch?.status === 'FINISHED') {
-                animeTime = 'The anime has ended, all we can do now is hope for season 2 and support the series in any and every way we can so that season 2 arrives faster.'
-            } else if (animeFetch?.status === 'RELEASING') {
-                if (animeFetch.nextAiringEpisode.timeUntilAiring && animeFetch.nextAiringEpisode.episode) {
-                    if (animeFetch.nextAiringEpisode.airingAt) {
-                        animeTime = `Episode ${animeFetch.nextAiringEpisode.episode} airs in <t:${Math.round(Date.now()/1000 + animeFetch.nextAiringEpisode.timeUntilAiring)}:R> at <t:${animeFetch.nextAiringEpisode.airingAt}:F>. The English subtitles usually release an hour later.`
-                    } else {
-                        animeTime = `Episode ${animeFetch.nextAiringEpisode.episode} airs in <t:${Math.round(Date.now()/1000 + animeFetch.nextAiringEpisode.timeUntilAiring)}:R>. The English subtitles usually release an hour later.`
-                    }
-                } else if (animeFetch.nextAiringEpisode.timeUntilAiring && !animeFetch.nextAiringEpisode.episode) {
-                    if (animeFetch.nextAiringEpisode.airingAt) {
-                        animeTime = `The next episode airs in <t:${Math.round(Date.now()/1000 + animeFetch.nextAiringEpisode.timeUntilAiring)}:R> at <t:${animeFetch.nextAiringEpisode.airingAt}:F>. The English subtitles usually release an hour later.`
-                    } else {
-                        animeTime = `The next episode airs in <t:${Math.round(Date.now()/1000 + animeFetch.nextAiringEpisode.timeUntilAiring)}:R>. The English subtitles usually release an hour later.`
-                    }
-                }
-            }                       
-        }
+        // let animeTime = 'The Japanese raws (episode without subtitles) release every Friday at 01:05 JST. The English subtitles usually release an hour later.'
+        // let animeFetch = await timeTillAiring()
+        // if (animeFetch) {
+        //     if (animeFetch?.status === 'FINISHED') {
+        //         animeTime = 'The anime has ended, all we can do now is hope for season 2 and support the series in any and every way we can so that season 2 arrives faster.'
+        //     } else if (animeFetch?.status === 'RELEASING') {
+        //         if (animeFetch.nextAiringEpisode.timeUntilAiring && animeFetch.nextAiringEpisode.episode) {
+        //             if (animeFetch.nextAiringEpisode.airingAt) {
+        //                 animeTime = `Episode ${animeFetch.nextAiringEpisode.episode} airs in <t:${Math.round(Date.now()/1000 + animeFetch.nextAiringEpisode.timeUntilAiring)}:R> at <t:${animeFetch.nextAiringEpisode.airingAt}:F>. The English subtitles usually release an hour later.`
+        //             } else {
+        //                 animeTime = `Episode ${animeFetch.nextAiringEpisode.episode} airs in <t:${Math.round(Date.now()/1000 + animeFetch.nextAiringEpisode.timeUntilAiring)}:R>. The English subtitles usually release an hour later.`
+        //             }
+        //         } else if (animeFetch.nextAiringEpisode.timeUntilAiring && !animeFetch.nextAiringEpisode.episode) {
+        //             if (animeFetch.nextAiringEpisode.airingAt) {
+        //                 animeTime = `The next episode airs in <t:${Math.round(Date.now()/1000 + animeFetch.nextAiringEpisode.timeUntilAiring)}:R> at <t:${animeFetch.nextAiringEpisode.airingAt}:F>. The English subtitles usually release an hour later.`
+        //             } else {
+        //                 animeTime = `The next episode airs in <t:${Math.round(Date.now()/1000 + animeFetch.nextAiringEpisode.timeUntilAiring)}:R>. The English subtitles usually release an hour later.`
+        //             }
+        //         }
+        //     }                       
+        // }
         if (mangaTime) {
-            rawChp = `${rawChp}\n**Timestamps for the release of the raws:** <t:${mangaTime.WednesdayTimestamp}:F>\n<t:${mangaTime.WednesdayTimestamp}:R>`
+            rawChp = `${rawChp}\n**Timestamps for the release of the raws:** <t:${mangaTime.WednesdayTimestamp}:F> <t:${mangaTime.WednesdayTimestamp}:R>`;
         }
         let embed = new MessageEmbed()
             .setAuthor({name: `${client.user.username}`, iconURL: `${client.user.displayAvatarURL({dynamic: true, size: 1024})}`})
-            .setColor(`${process.env.colour}`)            
+            .setColor(`${process.env.colour}`);
 
         if (!args[0]) {
-            embed.addField('Chapters Translated To English', 'The next chapter will be released when the TL team is done with it. There is no specific schedule which is followed for the release of translated English chapters. As this is not the only manga done by the team, it might take a while as compared to some other TL teams. The TL team appreciates your patience.')
-            embed.addField('Japanese Raw Chapters', `${rawChp}`)
-            embed.addField('Anime', `${animeTime}`)
+            embed.addFields({name: 'Chapters Translated To English', value: `Viz releases the chapter 7 hours after it\'s released in Japan.\n\n**Timestamps for the release of the translated chapter:** <t:${mangaTime.WednesdayTimestamp + 25200}:F>\n<t:${mangaTime.WednesdayTimestamp + 25200}:R>`}, 
+            {name: 'Japanese Raw Chapters', value: `${rawChp}`}, {name: "Anime", value: "**No season 2 has been announced.**"});
         } else if (args[0] === 'eng' || args[0] === 'en') {
-            embed.setTitle('No specific schedule is followed.')
-            embed.setURL('https://mangadex.org/title/259dfd8a-f06a-4825-8fa6-a2dcd7274230')
-            embed.setDescription('The next chapter will be released when the TL team is done with it. There is no specific schedule which is followed for the release of translated English chapters. As this is not the only manga done by the team, it might take a while as compared to some other TL teams. The TL team appreciates your patience.')
+            embed.setTitle('Viz releases the chapter 7 hours after it\'s released in Japan.');
+            embed.setURL('https://cubari.moe/read/gist/cmF3L3RoZWNsb2FrbWFzdGVyL3lvZnVrYXNoaS1uby11dGEvbWFpbi9tYWluLmpzb24/');
+            embed.setDescription(`**Timestamps for the release of the translated chapter:** <t:${mangaTime.WednesdayTimestamp + 25200}:F> <t:${mangaTime.WednesdayTimestamp + 25200}:R>`);
         } else if (args[0] === 'jp') {
             embed.setTitle('Wednesday at 00:00 JST')
             embed.setDescription(`${rawChp}`)
         } else if (args[0] === 'a' || args[0] === 'anime' || args[0] === 'episode' || args[0] === 'ep') {
-            embed.setTitle('Raws release every Friday at 00:55 / 1:05 JST')
-            embed.setDescription(`${animeTime}`)
-            if (animeFetch?.coverImage?.extraLarge) {
-                embed.setImage(`${animeFetch.coverImage.extraLarge}`)
-            }
-            if (animeFetch?.coverImage?.color) {
-                embed.setColor(`${animeFetch.coverImage.color}`)
-            }
+            embed.setTitle('No season 2 has been announced')
+            // embed.setDescription(`${animeTime}`)
+            // if (animeFetch?.coverImage?.extraLarge) {
+            //     embed.setImage(`${animeFetch.coverImage.extraLarge}`)
+            // }
+            // if (animeFetch?.coverImage?.color) {
+            //     embed.setColor(`${animeFetch.coverImage.color}`)
+            // }
         } else {
             return message.channel.send('No results for the specified arguments.')
         }
